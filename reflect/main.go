a52537147cf865e8829c7e614efa759e2ddd8471
Tachera Sasi <tacherasasi@gmail.com>
2025-09-23 08:36:04 +0300

    Ellie: add simple reflect example

--- reflect/main.go
+++ reflect/main.go
@@ -1,5 +1,9 @@
 package main
 
+import (
+	"reflect"
+)
+
 type Config struct {
     Username string `display:"-"`
     Password string `display:"mask"`
@@ -9,4 +13,17 @@ type Config struct {
 
 func main(){
 	println("hello reflect")
+	cfg := Config{
+        Username: "user123",
+        Password: "secretpass",
+        Age:      30,
+        Roles:    []string{"admin", "user"},
+    }
+
+    PrintStruct(cfg)
+}
+
+func PrintStruct(s any) {
+	val := reflect.ValueOf(s)
+	println("Type:", val.Type().Name())
 }
