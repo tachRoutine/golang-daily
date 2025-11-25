1dab136296d233c36de1b5927fa1be2c386a26f8
Tachera Sasi <tacherasasi@gmail.com>
2025-11-25 10:42:19 +0300

    implement RawInput function to read from stdin and output to stdout

--- io/main.go
+++ io/main.go
@@ -1,4 +1,25 @@
 package main
 
+import "os"
+
+// type Reader interface {
+//     Read(p []byte) (n int, err error)
+// }
+
+// type Writer interface {
+//     Write(p []byte) (n int, err error)
+// }
+
 func main() {
+	input := RawInput()
+	os.Stdout.WriteString(input)
+}
+
+func RawInput() string {
+	buf := make([]byte, 1024)
+	n,err := os.Stdin.Read(buf)
+	if err != nil {
+		return ""
+	}
+	return string(buf[:n])
 }
