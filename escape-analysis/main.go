193dcc9c74e15dbb4fa873fc6d01bb08c29898a3
tacheraSasi <tacherasasi@gmail.com>
2026-02-20 11:43:28 +0300

    Update main.go

--- escape-analysis/main.go
+++ escape-analysis/main.go
@@ -1,14 +1,37 @@
 package main
 
 func main(){
-	
+	println(willEscape().(string))
+	println(willNotEscape())
 }
 
-func willEscape() string {
+func willEscape() interface{} {
 	x := "Hello, World!"
 	return x
 }
 
 func willNotEscape() string {
-	return "Hello, World!"
+	x := "Hello, World!"
+	return x
+}
+
+func returnsPointer() *string {
+	x := "Hello, World!"
+	return &x
+}
+
+func returnsClosure() func() string {
+	x := "Hello, World!"
+	return func() string {
+		return x
+	}
+}
+
+
+
+func returnsClosurePointerPointer() **func() string {
+	x := "Hello, World!"
+	return &(&func() string {
+		return x
+	})
 }
