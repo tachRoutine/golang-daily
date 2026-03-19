c01efd2bf6b9a364153ccd8fbc6782d391f5e432
tacheraSasi <tacherasasi@gmail.com>
2026-03-19 14:26:58 +0300

    Implement concurrent web scraping using goroutines in web-scrap.go

--- concurrency/web-scrap.go
+++ concurrency/web-scrap.go
@@ -3,6 +3,7 @@ package main
 import (
 	"io"
 	"net/http"
+	"sync"
 )
 
 type Page struct {
@@ -17,5 +18,18 @@ func Scrape(url string) Page {
 }
 
 func main() {
+	urls := []string{"https://example.com", "https://google.com", "https://github.com"}
+
+	var wg sync.WaitGroup
+	result := make(chan Page, len(urls))
+
+	for _, url := range urls {
+		wg.Add(1)
+		go func(url string){
+			defer wg.Done()
+			result <- Scrape(url)
+		}(url)
+	}
+
 
 }
