a79b7f9a6100fbc9b617de88cb5324a8393f41fd
tacheraSasi <tacherasasi@gmail.com>
2026-06-02 11:09:12 +0300

    Add switch statement example in main.go

---
+++ switch/main.go
@@ -0,0 +1,22 @@
+package main
+
+func main() {
+	val := 2
+
+	switch val {
+	case 1:
+		{
+			println("One")
+		}
+	case 2:
+		{
+			println("Two")
+		}
+	case 3:
+		{
+			println("Three")
+		}
+	default:
+		println("Other")
+	}
+}
